dbd366b0c3925c48187f9010e3a6fedc0fc41e40
Patobeur <patobeur41@gmail.com>
2021-09-23 15:41:19 +0200

    fixe

--- js/loader.js
+++ js/loader.js
@@ -1,8 +1,8 @@
 
 console.log('v1')
 const TEMPLATE = 'client_1'
-const CLIENTIMGURL = '../themes/' + TEMPLATE + '/assets/img'
-const CLIENTJSURL = '../themes/' + TEMPLATE + '/js'
+const CLIENTIMGURL = 'themes/' + TEMPLATE + '/assets/img'
+const CLIENTJSURL = 'themes/' + TEMPLATE + '/js'
 window.onload = () => {
 	// definitions
 	const jsFilesList = [
