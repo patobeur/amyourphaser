bedc924052556bc6dd931e8815961dba630572ae
Patobeur <patobeur41@gmail.com>
2022-02-22 20:20:03 +0100

    no message

--- v1/js/gamedatas/main.js
+++
@@ -1,8 +0,0 @@
-// refresh scenes
-myPhaserConfig.scene = [SceneMain]
-// creation GAME Instance
-const LOGON = false
-let ITEMFACTORY = new ItemFactory();
-let UIFACTORY = new UiDomFactory();
-let PLAYERFACTORY = new PlayerFactory();
-let GAME = new Phaser.Game(myPhaserConfig);
